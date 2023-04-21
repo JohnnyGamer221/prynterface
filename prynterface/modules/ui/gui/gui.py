4bb9ab1f94df88436d94fcbe7f050343b2f7cc08
Dominik Ullrich <dominik119017@gmail.com>
2023-04-21 14:06:23 +0200

    edit todo

--- prynterface/modules/ui/gui/gui.py
+++ prynterface/modules/ui/gui/gui.py
@@ -2,8 +2,6 @@ import customtkinter
 
 
 # @todo implement GUI class [implement][tests]
-# - [ ] Class
-# - [ ] Tests
 # Gets config from UI Controller class, sets up GUI and runs it.
 class GUI:
     pass
